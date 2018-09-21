58a35b2ed47993349330c058668b91ad68a3edb6
chenweigao <297859260@qq.com>
2018-09-21 11:36:36 +0800

    sad

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    title: 'Hello VuePress',
+    description: 'Just playing around',
+    themeConfig: {
+        nav: [
+          { text: 'Home', link: '/' },
+          { text: 'Guide', link: '/guide/' },
+          { text: 'External', link: 'https://google.com' },
+        ]
+      }
+  }
