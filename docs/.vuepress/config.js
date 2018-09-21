8fe1b1d1fcd7c3afe3e77622e65ac739a2cf99ba
chenweigao <297859260@qq.com>
2018-09-21 14:50:47 +0800

    build the struct

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,83 @@
 module.exports = {
-    title: 'WGsite-2',
-    description: 'anothor site for work',
-    themeConfig: {
-        nav: [
-          { text: 'Home', link: '/' },
-          { text: 'Blog', link: '/blog/' },
-          { text: 'Docs', link: '/logs/' },
+  title: 'WEIGAO WORK',
+  description: 'anothor site for work',
+  // head: [
+  //   ['link', { rel: 'icon', href: '/dist/assets/img/author21.png'}]
+  // ],
+  markdown: {
+    lineNumbers: true
+  },
+  themeConfig: {
+    lastUpdated: 'Last Updated',
+    nav: [{
+        text: 'Home',
+        link: '/'
+      },
+      {
+        text: 'Web',
+        link: '/web/'
+      },
+      {
+        text: 'Tool',
+        link: '/tool/'
+      },
+      {
+        text: 'Docs',
+        link: '/logs/'
+      },
+      {
+        text: 'Code',
+        items: [{
+            text: 'C/C++',
+            link: '/algorithm/cpp/'
+          },
+          {
+            text: 'Leetcode',
+            link: '/algorithm/leetcode/'
+          }
         ]
+      },
+      {
+        text: 'About',
+        items: [{
+          text: 'Main Blog',
+          link: 'https://www.weigao.cc'
+        }, {
+          text: 'GitHub',
+          link: 'https://github.com/chenweigao'
+        }]
       }
+    ],
+
+    sidebar: {
+      '/algorithm/cpp/': [
+        '',
+        'c'
+      ],
+      // '/algorithm/leetcode/': [
+      //   '',
+      // ],
+      '/web/': [
+        '',
+        'a',
+        'b',
+      ],
+      'algorithm': [
+        '',
+        '/cpp/'
+      ],
+      '/': [
+        '',
+      ]
+    },
+    // sidebar : 'auto',
+    serviceWorker: {
+      updatePopup: true // Boolean | Object, 默认值是 undefined.
+      // 如果设置为 true, 默认的文本配置将是: 
+      // updatePopup: { 
+      //    message: "New content is available.", 
+      //    buttonText: "Refresh" 
+      // }
+    }
   }
+}
---
+++ docs/web/a.md
@@ -0,0 +1,8 @@
+# This is blog title
+## a
+aaaaaaaaaaa
+## b
+```python
+def fun():
+    return
+```
