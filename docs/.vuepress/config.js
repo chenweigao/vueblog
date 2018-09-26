51d560d360f902eb7b74091bf4c96566204a7b0e
chenweigao <297859260@qq.com>
2018-09-26 10:48:57 +0800

    add icons

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,12 +1,23 @@
 module.exports = {
   title: 'work.weigao.cc',
   description: 'anothor site for work',
+  head: [
+    ['link', { rel: 'icon', href: `/logo.png` }],
+    ['link', { rel: 'manifest', href: '/manifest.json' }],
+    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
+    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
+    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
+    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
+    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
+    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+  ],
   // head: [
   //   ['link', { rel: 'icon', href: '/dist/assets/img/author21.png'}]
   // ],
-  markdown: {
-    lineNumbers: true
-  },
+  // markdown: {
+  //   lineNumbers: true
+  // },
   themeConfig: {
     lastUpdated: 'Last Updated',
     nav: [{
