8a39e5604e232d0256018ff228701093b1a1ddf5
chenweigao <297859260@qq.com>
2018-09-29 12:05:25 +0800

    update and test

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,15 +2,43 @@ module.exports = {
   title: 'work.weigao.cc',
   description: 'anothor site for work',
   head: [
-    ['link', { rel: 'icon', href: `/logo.png` }],
-    ['link', { rel: 'manifest', href: '/manifest.json' }],
-    ['meta', { name: 'theme-color', content: '#3eaf7c' }],
-    ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
-    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }],
-    ['link', { rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png` }],
-    ['link', { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c' }],
-    ['meta', { name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png' }],
-    ['meta', { name: 'msapplication-TileColor', content: '#000000' }]
+    ['link', {
+      rel: 'icon',
+      href: `/logo.png`
+    }],
+    ['link', {
+      rel: 'manifest',
+      href: '/manifest.json'
+    }],
+    ['meta', {
+      name: 'theme-color',
+      content: '#3eaf7c'
+    }],
+    ['meta', {
+      name: 'apple-mobile-web-app-capable',
+      content: 'yes'
+    }],
+    ['meta', {
+      name: 'apple-mobile-web-app-status-bar-style',
+      content: 'black'
+    }],
+    ['link', {
+      rel: 'apple-touch-icon',
+      href: `/icons/apple-touch-icon-152x152.png`
+    }],
+    ['link', {
+      rel: 'mask-icon',
+      href: '/icons/safari-pinned-tab.svg',
+      color: '#3eaf7c'
+    }],
+    ['meta', {
+      name: 'msapplication-TileImage',
+      content: '/icons/msapplication-icon-144x144.png'
+    }],
+    ['meta', {
+      name: 'msapplication-TileColor',
+      content: '#000000'
+    }]
   ],
   // head: [
   //   ['link', { rel: 'icon', href: '/dist/assets/img/author21.png'}]
@@ -20,6 +48,7 @@ module.exports = {
   // },
   themeConfig: {
     lastUpdated: 'Last Updated',
+    // showScrollToTop: true,
     nav: [{
         text: 'Home',
         link: '/'
@@ -82,7 +111,7 @@ module.exports = {
         '/cpp/',
         '/leetcode/'
       ],
-      '/logs/':[
+      '/logs/': [
         '',
         'note'
       ],
@@ -97,6 +126,15 @@ module.exports = {
       //    message: "New content is available.", 
       //    buttonText: "Refresh" 
       // }
+    },
+    plugins: {
+      '@vuepress/i18n-ui': true,
+      '@vuepress/back-to-top': true,
+      '@vuepress/pwa': {
+        serviceWorker: true,
+        updatePopup: true
+      },
+      '@vuepress/plugin-medium-zoom': true
     }
-  }
+}
 }
