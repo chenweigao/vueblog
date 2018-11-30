ed3144d6455095445dfd849911cb8801c1f2765d
chenweigao <297859260@qq.com>
2018-11-30 12:23:05 +0800

    add button

---
+++ docs/.vuepress/components/HomeIndex.vue
@@ -0,0 +1,36 @@
+<template>
+  <el-row>
+    <el-button @click="toHome">Home</el-button>
+    <el-button
+      type="primary"
+      @click="toBlog"
+    >Blog</el-button>
+    <el-button type="success">Book</el-button>
+    <el-button type="info">Discuss</el-button>
+    <el-button type="warning">Docs</el-button>
+    <el-button type="danger">Code</el-button>
+    <el-button
+      type="info"
+      round
+    >About</el-button>
+  </el-row>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: []
+    }
+  },
+  methods: {
+    toHome() {
+      this.$router.push({ path: "/" })
+    },
+    toBlog() {
+      this.$router.push({ path: "/blog/" })
+    },
+  }
+}
+</script>
--- docs/README.md
+++ docs/README.md
@@ -3,6 +3,9 @@ sidebar: false
 footer: MIT Licensed | Copyright © 2018-present Weigao Chen
 ---
 <!-- <Card/> -->
-<Test2/>
-<Homepage/>
+<!-- <Test2/> -->
+<!-- <Homepage/> -->
 <!-- <welcome/> -->
+<HomeIndex/>
+
+我懒得写了，只有前两个能点Orz
