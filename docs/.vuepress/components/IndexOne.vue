afd55cc7c3e5e51b74d1f8eb2ef98aedda3ec589
chenweigao <297859260@qq.com>
2018-12-28 12:33:18 +0800

    make index auto read the v-for parament

--- docs/.vuepress/components/IndexOne.vue
+++ docs/.vuepress/components/IndexOne.vue
@@ -1,78 +1,73 @@
 <template>
-<div>
+  <div>
     <h1>2018</h1>
     <transition-group appear enter-active-class="fadeInUp">
-        <div v-for="post in posts18" :key=post.title class="animated">
-            <p :class="['thin', 'color']" > {{ post.lastUpdated }}
-                <router-link :to="post.path"> ### {{ post.title }} </router-link>
-            </p>
-        </div>
+      <div v-for="post in posts(2018)" :key="post.title" class="animated">
+        <p :class="['thin', 'color']">
+          {{ post.lastUpdated }}
+          <router-link :to="post.path">### {{ post.title }}</router-link>
+        </p>
+      </div>
     </transition-group>
-        <h1>2017</h1>
+    <h1>2017</h1>
     <transition-group appear enter-active-class="fadeInUp">
-
-        <div v-for="post in posts17" :key=post.key v-show=true class="animated">
-            <p :class="['thin', 'color']"> {{ post.lastUpdated }}
-                <router-link :to="post.path"> ### {{ post.title }} </router-link>
-            </p>
-        </div>
+      <div v-for="post in posts(2017)" :key="post.key" v-show="true" class="animated">
+        <p :class="['thin', 'color']">
+          {{ post.lastUpdated }}
+          <router-link :to="post.path">### {{ post.title }}</router-link>
+        </p>
+      </div>
     </transition-group>
-</div>
+  </div>
 </template>
 
 <script>
 export default {
-    data: function () {
-        return {
-            flag: false
-        }
+  data: function() {
+    return {
+      flag: false
+    };
+  },
+  methods: {
+    getTimestamp: function(time) {
+      return time.replace(/[^0-9]/gi, "");
     },
-    methods: {
-        getTimestamp: function (time) {
-            return time.replace(/[^0-9]/ig, "")
-        }
-    },
-    computed: {
-        posts18() {
-            // console.log(this.$site.pages);
-            return this.$site.pages
-                .filter(x => x.path.startsWith("/blog/2018/") && !x.frontmatter.blog_index)
-                .sort((a, b) => Date.parse(b.lastUpdated) - Date.parse(a.lastUpdated))
-        },
-        posts17() {
-            // console.log(this.$site.pages);
-            return this.$site.pages
-                .filter(x => x.path.startsWith("/blog/2017/") && !x.frontmatter.blog_index)
-                .sort((a, b) => Date.parse(b.lastUpdated) - Date.parse(a.lastUpdated))
-        }
+    posts: function(n) {
+      var postDir = "/blog/" + n + "/";
+      return this.$site.pages
+        .filter(x => x.path.startsWith(postDir) && !x.frontmatter.blog_index)
+        .sort((a, b) => Date.parse(b.lastUpdated) - Date.parse(a.lastUpdated));
     }
+  },
+  computed: {
+  }
 };
 </script>
 
 <style>
 .thin {
-    font-weight: 300;
+  font-weight: 300;
 }
 
 .size {
-    font-size: 0.83em;
+  font-size: 0.83em;
 }
 
 .color {
-    color: black;
-    /* color: dodgerblue; */
+  color: black;
+  /* color: dodgerblue; */
 }
 
 .v-enter,
 .v-leave-to {
-    opacity: 0;
-    transform: translateX(50px);
+  opacity: 0;
+  transform: translateX(50px);
 }
 
 .v-enter-active,
 .v-leave-active {
-    transition: all 0.6s ease;
+  transition: all 0.6s ease;
 }
 
-@import 'https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css'
+@import "https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css";
 </style>
