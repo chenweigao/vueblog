0aa70e4f328d80b77a512dcaec385fe02f6a95d5
chenweigao <297859260@qq.com>
2019-05-10 16:00:16 +0800

    remove mycss

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,6 +1,6 @@
 import ElementUI from "element-ui";
 import 'element-ui/lib/theme-chalk/index.css'
-import './styles/mycss.css'
+// import './styles/mycss.css'
 import animated from 'animate.css'
 import VueRouter from "vue-router";
 // import SearchBox from "@vuepress/plugin-search"
